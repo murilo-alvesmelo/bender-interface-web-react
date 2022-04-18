8b167217d4373f566bce8de8b46819e7ca36ee82
Murilo Alves <murilo.melo72@hotmail.com>
2022-04-18 14:46:26 -0300

    R48/pagina-inserir-dados-web

--- src/App.js
+++ src/App.js
@@ -6,8 +6,6 @@ import {
 } from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import styles from './App.module.css'
-import CadastroCurso from './componentes/cadastroCurso/'
-import TelaInicial from './componentes/telaInicial'
 import InserirInfo from './componentes/inserirInformacoes'
 import NavBar from './layout/NavBar';
 
@@ -16,8 +14,7 @@ function App() {
     <div className={styles.App}>
     <Router>
       <Routes>
-        <Route path="/Cadastro_curso" element={<CadastroCurso/>} />
-        <Route path="/tela_inicial" element={<TelaInicial/>} />
+        <Route path="/inserir_info" element={<InserirInfo/>} />
       </Routes>
     </Router>
     </div>
