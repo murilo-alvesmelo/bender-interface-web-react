69412014e06194ad4a54f665ceabaa58a896b314
Murilo Alves <murilo.melo72@hotmail.com>
2022-05-09 15:25:46 -0300

    R99/Tela_cadastro_instagrams_curso

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import styles from './App.module.css'
 import TelaInicial from './componentes/telaInicial'
 import EditarInfo from './componentes/editarInformacoes';
 import InserirInfo from './componentes/inserirInformacoes'
-import NavBar from './layout/NavBar';
+import InstagramCurso from './componentes/cadastroInstagramCurso';
 
 function App() {
   return (
@@ -19,6 +19,7 @@ function App() {
         <Route path="/" element={<TelaInicial/>} />
         <Route path="/editar_info" element={<EditarInfo/>} />
         <Route path="/inserir_info" element={<InserirInfo/>} />
+        <Route path="/inserir_instagram" element={<InstagramCurso/>} />
       </Routes>
     </Router>
     </div>
