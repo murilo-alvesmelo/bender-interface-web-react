b992df87ebedb9f6c0db76f5e9b6878bf325c71d
Murilo Alves Melo <83835393+murilo-alvesmelo@users.noreply.github.com>
2022-06-14 15:18:01 -0300

    Update Constantes.js

--- src/utilitarios/Constantes.js
+++ src/utilitarios/Constantes.js
@@ -1,2 +1,2 @@
-export const URL_BASE = 'http://127.0.0.1:8000';
-//export const URL_BASE = 'https://bender-api-uft.herokuapp.com';
+// export const URL_BASE = 'http://192.168.31.118:8000';
+export const URL_BASE = 'https://bender-api-uft.herokuapp.com';
