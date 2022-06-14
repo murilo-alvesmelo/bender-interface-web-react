45c4b84fcaa235e24ed1822b9555c824acad5875
Murilo Alves <murilo.melo72@hotmail.com>
2022-06-14 14:39:11 -0300

    Manutenção tela NavBar e tela inicial

--- src/layout/NavBar.js
+++ src/layout/NavBar.js
@@ -6,6 +6,7 @@ import Logo from '../assets/logo.jpg'
 import styles from './NavBar.module.css'
 import Button from 'react-bootstrap/Button'
 import {useNavigate} from "react-router-dom"
+
 const NavBar = ()=>{
         const navigate = useNavigate();
         const handleLogout = ()=>{
@@ -17,22 +18,21 @@ const NavBar = ()=>{
             <Navbar style={{backgroundColor: '#253341'}}variant='dark'>
                 <Container>
                     <figure className={styles.imgPosition}>
-                        <img src={Logo} className={styles.logo} alt="logo"/>
+                        <img src={Logo} className={styles.logo}/>
                     </figure>
                     <Navbar.Brand href="/">Bender Web</Navbar.Brand>
                     <Nav className="me-auto">
                         <div className={styles.navLinks}>
                         <Nav.Link href="/" className={styles.text}>Inicio</Nav.Link>
                         <Nav.Link href="/inserir_info" className={styles.text}>Inserir informações</Nav.Link>
-                        <Nav.Link href="/editar_info" className={styles.text}>Informações Personalizadas</Nav.Link>
-                        <Nav.Link href="/editar_info" className={styles.text}>Editar Informações</Nav.Link>
+                        <Nav.Link href="/editar_info" className={styles.text}>Editar informações</Nav.Link>
                         </div>
                         <Button variant="outline-secondary" className={styles.button} onClick={handleLogout}>Sair</Button>{' '}
                     </Nav>
                 </Container>
             </Navbar>
-            </>    
-            )
+            </>
+        )
     }
 
 export default NavBar;
