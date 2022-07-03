1db408d3693c40cc6db692525b4f9fca1125bc25
Murilo Alves <murilo.melo72@hotmail.com>
2022-07-03 20:08:23 -0300

    Adicionando campos de PDF

--- src/componentes/inserirInformacoes/index.js
+++ src/componentes/inserirInformacoes/index.js
@@ -9,7 +9,12 @@ const InserirInfo = () => {
       <NavBar />
       <div className={styles.container}>
         <span className={styles.title}>Inserir Informações do Curso</span>
+        <Box title = 'Auxilios UFT' rota= "/informacoes_curso/auxilio_uft"/>
+        <Box title = 'Biblioteca UFT' rota= "/informacoes_curso/biblioteca_uft"/>
         <Box title = 'Calendario Acadêmico' rota= "/informacoes_curso/calendario_academico"/>
+        <Box title = 'Matriz curricular' rota= "/informacoes_curso/matriz_curricular"/>
+        <Box title = 'Regimento Acadêmico' rota= "/informacoes_curso/regimento_academico"/>
+        <Box title = 'Restaurante Universitário' rota= "/informacoes_curso/restaurante_universitario"/>
       </div>
     </>
   );
